f40c220e96c6420f7f75ce573c84468e985990a6
Mixter213 <determine18@gmail.com>
2023-12-31 08:33:14 -0500

    Initial Test

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -1,16 +1,13 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
 	meta: {
-		name: "Steam",
-		author: "maenDisease",
-		authorLink: "https://maendisease.github.io/",
+		name: "MixSteam",
+		author: "Mixter213 maenDisease",
+		// authorLink: "https://maendisease.github.io/",
 		version: "1.0.0",
-		description: "Steam UI on Discord.",
+		description: "Me Learning stuff n things from Steam UI on Discord. ",
 		source: "https://github.com/maenDisease/Steam",
 		website: "https://maendisease.github.io/",
-		invite: "BShu37e4jg"
-	},
-	addons: [
-		['./src/addons/_horizontalserverlist.scss', 'dist/addons/horizontalserverlist.css'],
-	]
+		// invite: "BShu37e4jg"
+	}
 }
